e4576734f6920b2d17bbdaa6f63a7223a062278f
rcg12 <rcg121294@outlook.com>
2025-03-05 21:35:49 +0100

    Add hibernate package

--- src/main/java/Rafa/main/DAOFactory.java
+++ src/main/java/Rafa/main/DAOFactory.java
@@ -1,6 +1,6 @@
 package Rafa.main;
 
-//import Ainara.db4o.Db4oDAO;
+import Ainara.db4o.Db4oDAO;
 import Ainara.mongoDB.MongoDBDAO;
 import DAO.IDAO;
 import Pere.file.TextFileDAO;
@@ -13,7 +13,7 @@ public class DAOFactory {
             case 1:
                 return new HibernateDAO();
             case 2:
-//                return new Db4oDAO(); // Implementación de Db4o
+                return new Db4oDAO(); // Implementación de Db4o
             case 3:
                 return new MongoDBDAO(); // Implementación de MongoDB
             case 4:
